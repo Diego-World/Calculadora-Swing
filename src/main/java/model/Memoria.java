18e78c9fee19ba313a8eb0b32ff4e21e24e23257
diegoDev <contato.diegogouveia@gmail.com>
2024-02-08 10:58:15 -0300

    Metódo para detectar o tipo de comando

--- src/main/java/model/Memoria.java
+++ src/main/java/model/Memoria.java
@@ -5,6 +5,10 @@ import java.util.List;
 
 public class Memoria {
 
+    private enum TipoComando {
+            ZERAR, NUMERO, DIV, MULT, SUB, SOMA, IGUAL, VIRGULA
+    };
+
     private static final Memoria instancia = new Memoria();
     private final List<MemoriaObservador> observadores = new ArrayList<>();
     private String textoAtual = "";
@@ -26,12 +30,38 @@ public class Memoria {
         return textoAtual.isEmpty() ? "0" : textoAtual;
     }
 
-    public void processarComando(String valor){
-        if("AC".equals(valor)){
-            textoAtual = "";
-        }else{
-            textoAtual += valor;
-        }
+    public void processarComando(String texto){
+
+        TipoComando tipoComando = detectarTipoComando(texto);
+        System.out.println(tipoComando);
         observadores.forEach(o-> o.valorAlterado(getTextoAtual()));
+    }
+
+    private TipoComando detectarTipoComando(String texto) {
+        if(texto.isEmpty() && texto == "0"){
+            return null;
+        }
+
+        try {
+            Integer.parseInt(texto);
+            return TipoComando.NUMERO;
+        }catch (NumberFormatException e){
+            if("AC".equals(texto)){
+                return TipoComando.ZERAR;
+            }else if("/".equals(texto)){
+                return TipoComando.DIV;
+            }else if("*".equals(texto)){
+                return TipoComando.MULT;
+            }else if("+".equals(texto)){
+                return TipoComando.SOMA;
+            }else if("-".equals(texto)){
+                return TipoComando.SUB;
+            }else if("=".equals(texto)){
+                return TipoComando.IGUAL;
+            }else if(",".equals(texto)){
+                return TipoComando.VIRGULA;
+            }
+        }
+        return null;
     }
 }
